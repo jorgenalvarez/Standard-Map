a11d7819f5fe26138b1a7954178d795bf1984686
jorgenalvarez <nicolasalvarez.jorge@gmail.com>
2020-01-04 21:05:52 +0100

    Manifold edit

--- manifold.c
+++ manifold.c
@@ -3,8 +3,8 @@
 
 double MU;
 
-void standard_map (double t, double x[], double f[])
+void standard_map (double x[])
 {
-	f[1] = x[1] - MU*sin(x[0]);
-	f[0] = x[0] + f[1];
+	x[1] = x[1] - MU*sin(x[0]);
+	x[0] = x[0] + x[1];
 }
